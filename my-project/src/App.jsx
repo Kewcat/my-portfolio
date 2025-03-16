04ce48ce8eccdf97628ce03fa2c02add9d07efac
Khushi <dovalkhushi@gmail.com>
2025-03-16 17:13:51 -0400

    Initial commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar.jsx'; 
+
+export default function App() {
+  return (
+    <div className="overflow-x-hidden text-stone-300 antialiased">
+      {/* Background gradient circles */}
+      <div className="fixed inset-0 -z-10">
+        <div className="relative h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        </div>
+      </div>
+
+      {/* Main content */}
+      <div className="container mx-auto px-8"> 
+        <Navbar /> {/* Navbar component */}
+      </div>
+    </div>
+  );
+};
+
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import logo from "/src/assets/raviKumarLogo.webp";
+import {FaGithub, FaLinkedin} from "react-icons/fa"
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">   
+          <img src={logo} className="mx-2" width={50} height={33} alt="Logo"/>
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/khushidoval001" target ="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+        <FaLinkedin/>
+
+        </a>
+        <a href="https://github.com/Kewcat" target ="_blank" rel="noopener noreferrer" aria-label="Github">
+        <FaGithub/>
+
+        </a>
+      </div>
+
+ 
+    </nav>
+  );
+};
+
+
