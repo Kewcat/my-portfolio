9c69b8f0205355dc5b91661698c63fb2168b3154
Khushi <dovalkhushi@gmail.com>
2025-03-16 17:41:08 -0400

    Update Hero.jsx
    Added Content in Hero

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -6,7 +6,21 @@ const Hero =() => {
             <div className='flex flex-wrap lg:flex-row-reverse'> 
                 <div className="w-full lg:w-1/2">
                 <div className='flex justify-center lg:p-8'>
-                    <img src={profilePic}></img> </div>
+                    <img src={profilePic} alt="space" className='border border-stone-900 rounded-3xl'></img> </div>
+                </div>
+                <div className='w-full lg:w-1/2'>
+                <div className='flex flex-col items-center lg:items-start mt-10'>
+                    <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Khushi Doval</h2>
+                    <span className='bg-gradient-to-r from-stone-200 to-stone-600 bg-clip-text text-3xl tracking-tight
+                    text-transparent'> Data Scientist
+                    </span>
+                    <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
+                    I thrive on solving complex problems with data and uncovering insights that drive real impact. With a strong foundation in Python, Java, and machine learning, I enjoy working at the intersection of AI, analytics, and software development.
+Currently, I’m seeking internship opportunities to apply my skills in data science, AI, and cloud computing, while learning from hands-on experience. I believe in continuous growth, collaboration, and building solutions that matter.
+Let’s connect and explore opportunities to create something meaningful!
+                    </p>
+                    
+                </div>
                 </div>
                 </div> 
                 </div>
